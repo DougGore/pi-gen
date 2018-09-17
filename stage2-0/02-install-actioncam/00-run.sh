36ae3f29016958a947c13acfb6993499e1a3a42f
Douglas Gore <doug@debian>
2018-09-17 21:38:26 +0100

    Directory shuffle

--- stage2-0/02-install-actioncam/00-run.sh
+++ stage2-0/02-install-actioncam/00-run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -e
 
 install -m 644 files/actioncam.service "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 files/smb.conf "${ROOTFS_DIR}/etc/samba/"
 
 on_chroot << EOF
+echo i2c-dev >> /etc/modules
+
+mkdir -m 1777 /share
+chown pi:pi /share
+smbpasswd -n -a pi
+
 systemctl enable actioncam.service
 EOF
