dcb52f1ded036624a3ceb8a47f73408a10a19bf3
Douglas Gore <doug@debian>
2019-04-25 01:00:51 +0100

    Implement self-hosted WiFi AP, USB serial and a few other custom tweaks

---
+++ build_actioncam_image.sh
@@ -0,0 +1,5 @@
+# Example for building a lite system
+echo "IMG_NAME='Actioncam'" > config
+touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
+touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
+sudo ./build.sh  # or ./build-docker.sh
--- stage2-0/02-install-actioncam/00-run.sh
+++ stage2-0/02-install-actioncam/00-run.sh
@@ -1,14 +1,27 @@
 #!/bin/bash -e
 
 install -m 644 files/actioncam.service "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 files/actioncam_usb.service "${ROOTFS_DIR}/lib/systemd/system/"
+install -d "${ROOTFS_DIR}/usr/lib/actioncam"
+install -m 755 files/setup_usb "${ROOTFS_DIR}/usr/lib/actioncam/"
 install -m 644 files/smb.conf "${ROOTFS_DIR}/etc/samba/"
+install -m 644 files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/"
+install -m 644 files/70-persistent-net.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
+
+# WiFi AP setup
+cat files/interfaces >> "${ROOTFS_DIR}/etc/network/interfaces"
+cat files/dnsmasq.conf >> "${ROOTFS_DIR}/etc/dnsmasq.conf"
+sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' "${ROOTFS_DIR}/etc/default/hostapd"
+#sed -i '19i/sbin/ifup wlan0' "${ROOTFS_DIR}/etc/rc.local"
 
 on_chroot << EOF
 echo i2c-dev >> /etc/modules
+echo libcomposite >> /etc/modules
 
 mkdir -m 1777 /share
 chown pi:pi /share
 smbpasswd -n -a pi
 
 systemctl enable actioncam.service
+systemctl enable actioncam_usb.service
 EOF
